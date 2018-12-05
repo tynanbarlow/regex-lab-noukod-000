ff4f7e67f4380b3d02f5244bfceda71cf4a4c048
Tynan Barlow <tynan@noukod.org>
2018-12-05 14:19:47 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  /[a,e,i,o,u]/
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  //
 end
 
 def words_five_letters_long(text)
-
+  text.grep(/^\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  //
 end
 
 def valid_phone_number?(phone)
-
+  //
 end
