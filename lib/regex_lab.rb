9ad7284dd3c32ce0a2e60c0cddab130d0fb94abd
Tynan Barlow <tynan@noukod.org>
2018-12-06 01:42:12 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,23 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[aAeEiIoOuU]/)
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  word_array = text.match(/(\w?un)(\w+ing)/)
 end
 
 def words_five_letters_long(text)
-
+  text.grep(/^\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  (//)
 end
 
 def valid_phone_number?(phone)
-
+  (//)
 end
